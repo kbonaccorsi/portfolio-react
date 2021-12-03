f3e6fbe6cb37db31a7d6c90b0409840316424021
Kaileigh Bonaccorsi <kaileigh.bonaccorsi@gmail.com>
2021-12-03 13:43:00 -0600

    folder structure created

---
+++ portfolio-react/src/Components/Nav/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Nav() {
+
+  return (
+    <div className="">
+      <h3 className="">
+        Kaileigh
+      </h3>
+      <div className="">
+        <p className="" style={{ fontSize: '50px' }}>Homepage</p>
+        <p className="" style={{ fontSize: '50px' }}>About Me</p>
+        <p className="" style={{ fontSize: '50px' }}>Contact</p>
+      </div>
+    </div>
+  );
+} 
