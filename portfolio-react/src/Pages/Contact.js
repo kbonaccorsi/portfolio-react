9d0b5068c10d3e561b1cd6fa9b2bda8e050f15ae
Kaileigh Bonaccorsi <kaileigh.bonaccorsi@gmail.com>
2022-02-17 10:44:09 -0600

    contact form works and styled

--- portfolio-react/src/Pages/Contact.js
+++ portfolio-react/src/Pages/Contact.js
@@ -3,14 +3,14 @@ import emailjs from '@emailjs/browser';
 import Swal from 'sweetalert2';
 
 
-const SERVICE_ID = process.env.REACT_APP_SERVICE_ID;
-const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID;
-const USER_ID = process.env.REACT_APP_USER_ID;
+// const SERVICE_ID = process.env.REACT_APP_SERVICE_ID;
+// const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID;
+// const USER_ID = process.env.REACT_APP_USER_ID;
 
 
 export default function Contact() {
   const form = useRef();
-  
+
   const handleOnSubmit = (e) => {
     e.preventDefault();
 
@@ -36,50 +36,14 @@ export default function Contact() {
     <div className="page-container">
       <h2 className="page-heading">Contact Me</h2>
       <div className="page-content d-flex">
-        <div className="form d-flex">
-          <form ref={form} onSubmit={handleOnSubmit}>
-            <label name="user_name" />          <input type="text" name="user_name" placeholder="name" icon='mail' iconPosition='left'/>
-            <label name="user_email" />          <input type="" name="user_email" placeholder="email" />
-            <label name="user_message" />
-            <textarea name="user_message" placeholder="message"/>
-            <input type="submit" value="Send" />
-          </form>
-        </div>
-
-        {/* 
-          <Form ref={form} onSubmit={handleOnSubmit}>
-            <form.Field
-              id='form-input-control-email'
-              control={Input}
-              label='Email'
-              name='user_email'
-              placeholder='Email...'
-              required
-              icon='mail'
-              iconPosition='left'
-            />
-            <form.Field
-              id='form-input-control-last-name'
-              control={Input}
-              label='Name'
-              name='user_name'
-              placeholder='Name...'
-              required
-              icon='user circle'
-              iconPosition='left'
-            />
-            <form.Field
-              id='form-textarea-control-opinion'
-              control={TextArea}
-              label='Message'
-              name='user_message'
-              placeholder='Message...'
-              required
-            />
-            <Button type='submit' className='btn'>Submit</Button>
-          </Form>
-        </div> */}
+        <form ref={form} onSubmit={handleOnSubmit} className="form d-flex">
+          <label name="user_name" />          <input type="text" name="user_name" placeholder="name" />
+          <label name="user_email" />          <input type="email" name="user_email" placeholder="email" />
+          <label name="user_message" />
+          <textarea name="user_message" placeholder="message" />
+          <input type="submit" value="Send" />
+        </form>
       </div>
-    </div>
+    </div >
   );
 };
